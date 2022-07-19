11ef7e3193d0a660d813986821f5eaeb6efd26d2
Sergey Naumov <drmgc@yandex.ru>
2022-07-19 14:57:08 +0300

    add: error handling

--- backend.ts
+++ backend.ts
@@ -1,6 +1,7 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
+import { HttpException, BadRequestException } from './http-exception'
 
 export const bootstrap = (port: number) => {
   const app: Express = express();
@@ -13,6 +14,16 @@ export const bootstrap = (port: number) => {
   app.get('/', (_: Request, res: Response) => {
     res.sendFile(__dirname + '/tracker.js')
   });
+
+  app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+    if (!(err instanceof HttpException)) return next(err)
+
+    res.status(err.statusCode)
+    res.json({
+      statusCode: err.statusCode,
+      message: err.message,
+    })
+  })
 
   app.listen(port, () => {
     console.log(`Backend is running at http://localhost:${port}`);
