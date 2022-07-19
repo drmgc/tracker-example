f23783a2aceca32a73a080136085ff1c30fee1e2
Sergey Naumov <drmgc@yandex.ru>
2022-07-19 14:57:19 +0300

    add: stub of /track endpoint

--- backend.ts
+++ backend.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
 import { HttpException, BadRequestException } from './http-exception'
+import { TrackerEvent, validateTrackerEvent } from './tracker-event'
 
 export const bootstrap = (port: number) => {
   const app: Express = express();
@@ -13,6 +14,24 @@ export const bootstrap = (port: number) => {
 
   app.get('/', (_: Request, res: Response) => {
     res.sendFile(__dirname + '/tracker.js')
+  });
+
+  app.post('/track', cors({
+    methods: 'POST',
+    // TODO: maxAge to avoid CORS preflight
+  }), (req: Request, res: Response) => {
+    if (!Array.isArray(req.body)) throw new BadRequestException('Array of events was expected')
+
+    req.body.forEach((e: Partial<TrackerEvent>, i: number) => {
+      const [ok, problems] = validateTrackerEvent(e)
+
+      if (!(ok)) throw new BadRequestException(`Event #${i}: ${problems}`)
+    })
+
+    const events: TrackerEvent[] = req.body
+
+    res.sendStatus(200);
+    console.log('tracking', events);
   });
 
   app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
